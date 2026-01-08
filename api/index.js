a26d49529e249150b1a4a64e41876e1764cad8cb
Jayesh435 <jayeshkaremore07@gmail.com>
2026-01-08 21:46:00 +0530

    feat(api): initialize API with user authentication routes and dependencies
    - Added package.json for API with necessary dependencies including express, mongoose, and jsonwebtoken.
    - Implemented user registration and login routes in auth.js, utilizing CryptoJS for password encryption and JWT for access token generation.

---
+++ api/index.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const authRoute = require("./routes/auth");
+const cors = require("cors");
+
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log("DB Connection Successful!"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
+
+app.get("/", (req, res) => {
+    res.send("Backend is running");
+});
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
